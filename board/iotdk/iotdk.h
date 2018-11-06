b4f8ca0204cfc58800e716655e47f451ea799fbc
Yiping Peng <yibingp@synopsys.com>
2018-11-06 17:57:18 +0800

    src: modify the memory layout of iotdk making bootloader running in on-chip flash
    Signed-off-by: Yiping Peng <yibingp@synopsys.com>

--- board/iotdk/iotdk.h
+++ board/iotdk/iotdk.h
@@ -160,7 +160,7 @@
 #define BOARD_SWT_MASK			(0x0)
 #define BOARD_SWT_CNT			(0)
 
-#define button_read(x) 			1
+#define button_read(x) 			0
 #define led_write(x, y)			EMBARC_PRINTF("led out: %x, %x\r\n", x, y)
 #define led_read(mask)			(0x0)
 #define led_toggle(mask)
